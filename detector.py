17b4fba5d1601a9c548b16490fb1e2ed2e0c798c
Senthil Kumar <132041836+Kumar2421@users.noreply.github.com>
2024-04-27 11:25:11 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -29,19 +29,47 @@ class FrameProcessor:
         self.face_detector.deploy(device)
         self.landmarks_detector.deploy(device, self.QUEUE_SIZE)
         self.face_identifier.deploy(device, self.QUEUE_SIZE)
-        self.faces_database = FacesDatabase("C:\\xampp1\\htdocs\\demo\\captured_faces", self.face_identifier, self.landmarks_detector)
+        self.faces_database = FacesDatabase("C:\\xampp1\\htdocs\\demo\\face_recogition_intel\\face_img1", self.face_identifier, self.landmarks_detector)
         self.face_identifier.set_faces_database(self.faces_database)
         log.info('Database is built, registered {} identities'.format(len(self.faces_database)))
-       
+        self.detections = []
+        self.face_tracks = {}  # Dictionary to store face tracks
+        self.next_track_id = 0  # Counter for assigning new track IDs
+
+    def update_face_tracks(self, detections):
+        # Update face tracks based on new detections
+        for detection in detections:
+            roi, landmarks, identity = detection
+            centroid = (roi.position[0] + roi.size[0] / 2, roi.position[1] + roi.size[1] / 2)
+            matched_track_id = None
+            min_distance = float('inf')
+            for track_id, track_centroid in self.face_tracks.items():
+                distance = ((centroid[0] - track_centroid[0]) ** 2 + (centroid[1] - track_centroid[1]) ** 2) ** 0.5
+                if distance < min_distance:
+                    min_distance = distance
+                    matched_track_id = track_id
+            if matched_track_id is not None and min_distance < 80:  # Threshold for centroid proximity
+                self.face_tracks[matched_track_id] = centroid
+                identity.id = matched_track_id
+                print(matched_track_id,"matched id")
+            else:
+                self.next_track_id += 1
+                self.face_tracks[self.next_track_id] = centroid
+                identity.id = self.next_track_id
+                print(self.next_track_id,"next id")
+
+
+      # Initialize detections attribute
+
     def face_process(self, frame):
         rois = self.face_detector.infer((frame,))
         if self.QUEUE_SIZE > len(rois):
             rois = rois[:self.QUEUE_SIZE]
         landmarks = self.landmarks_detector.infer((frame, rois))
         face_identities, _ = self.face_identifier.infer((frame, rois, landmarks))
-        # Combine detections
-        detections = list(zip(rois, landmarks, face_identities))
-        return detections
+        self.detections = list(zip(rois, landmarks, face_identities))  # Store detections
+        self.update_face_tracks(self.detections)  # Update face tracks
+        return self.detections
 
 # Modify the draw_face_detection function to handle multiple detections
 def draw_face_detection(frame_processor):
@@ -77,18 +105,16 @@ def image_recognizer(frame, text, identity, face_point, threshold):
 cap = cv2.VideoCapture(0)
 frame_processor = FrameProcessor()
 metrics = PerformanceMetrics()
-
 while True:
     start_time = perf_counter()
     ret, frame_processor.frame = cap.read()
     if not ret:
         break
-    frame_processor.detections = frame_processor.face_process(frame_processor.frame)
+    detections = frame_processor.face_process(frame_processor.frame)
     frame_processor.frame = draw_face_detection(frame_processor)
     metrics.update(start_time, frame_processor.frame)
     cv2.imshow("face recognition demo", frame_processor.frame)
     key = cv2.waitKey(1)
     if key in (ord('q'), ord('Q'), 27):
-        break
-
+            break
 
